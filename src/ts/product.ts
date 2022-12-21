7e8cbe08bd0c669e5e3c5e45fac15b03a9cebee9
Mikael Rönnberg <mikaelronnberg@Mikaels-MacBook-Pro.local>
2022-12-21 14:12:07 +0100

    la till lite uppdatering på produktsidan

--- src/ts/product.ts
+++ src/ts/product.ts
@@ -11,23 +11,28 @@ function createProducts(products: IProduct[]) {
 
     for(let i = 0; i < products.length; i++) {
         let aProduct: HTMLDivElement = document.createElement("div");
-        let bild: HTMLImageElement = document.createElement("img");
+        aProduct.classList.add("product-object");
+        let bild: HTMLDivElement = document.createElement("div");
         let namn: HTMLHeadingElement = document.createElement("h4");
         let kategori: HTMLParagraphElement = document.createElement("p");
         let beskrivning: HTMLParagraphElement = document.createElement("p");
         let pris: HTMLParagraphElement = document.createElement("p");
+        let knapp: HTMLButtonElement = document.createElement("button");
 
         namn.innerHTML = products[i].Name;
-        bild.innerHTML = products[i].Img;
+        bild.innerHTML = `<img src="${products[i].Img}" alt="${products[i].Name}">`;
+        
         kategori.innerHTML = products[i].Category;
         beskrivning.innerHTML = products[i].Description
-        pris.innerHTML = products[i].Price.toString();
-
+        pris.innerHTML = products[i].Price.toString() + " kr";
+        knapp.innerHTML = "Lägg i varukorgen";
+-
         aProduct.appendChild(namn);
         aProduct.appendChild(bild);
         aProduct.appendChild(pris);
         aProduct.appendChild(kategori);
         aProduct.appendChild(beskrivning);
+        aProduct.appendChild(knapp);
 
         (document.getElementById("product-container") as HTMLDivElement).appendChild(aProduct);
 
--- src/ts/services/mockProducts.ts
+++ src/ts/services/mockProducts.ts
@@ -1,9 +1,11 @@
 import { IProduct } from "../models/IProduct";
 
+// Kategorier: Dryck, Bröd, Kött, Vego, Sallad, Fisk, Sött
+
 export let mockData: IProduct[] = [
     {
         Name: "Julmust",
-        Img: "url",
+        Img: "../assets/Julmust.jpeg",
         Category: "Dryck",
         Price: 10.90,
         Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
@@ -11,8 +13,36 @@ export let mockData: IProduct[] = [
     {
         Name: "Julskinka",
         Img: "url",
-        Category: "Dryck",
+        Category: "Kött",
         Price: 50.90,
         Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
-    }
+    },
+    {
+        Name: "Rödbetssallad",
+        Img: "url",
+        Category: "Sallad",
+        Price: 50.90,
+        Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
+    },
+    {
+        Name: "Prinskorv",
+        Img: "url",
+        Category: "Kött",
+        Price: 50.90,
+        Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
+    },
+    {
+        Name: "Köttbullar",
+        Img: "url",
+        Category: "Kött",
+        Price: 50.90,
+        Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
+    },
+    {
+        Name: "Köttbullar",
+        Img: "url",
+        Category: "Kött",
+        Price: 50.90,
+        Description: "En härlig klassiker som står på sig varje jul. Ett måste till julbordet och för den nyktre personen. Kul trivia: varje jul sjunker Coca Colas försäljning då den omåttligt populäre konkurrenten Julmust tar över marknaden"
+    },
 ];
