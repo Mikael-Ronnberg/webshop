4c03f178efb70493c24ba0d86d3cf9a61802ff30
Mikael Rönnberg <mikaelronnberg@Mikaels-MacBook-Pro.local>
2022-12-28 23:25:51 +0100

    Så ja, mer struktur på allt i hela projektet. Sammanflätat stylingen och fixat länkarna i navigationen

--- src/ts/services/mockProducts.ts
+++ src/ts/services/mockProducts.ts
@@ -111,4 +111,148 @@ export let mockData: IProduct[] = [
         Quantity: 1,       
         Description: "lorem"
     },
+    {
+        Id: "13",
+        Name: "Köttbullar",
+        Img: "https://crockpot.se/wp-content/uploads/2021/04/kottbulla_web-640x480.jpg",
+        Category: "Kött",
+        Price: 50.90,
+        Quantity: 1,       
+        Description: "Lorem"
+    },
+    {
+        Id: "14",
+        Name: "Lucia Katter",
+        Img: "https://www.recepten.se/bilder/recept/2/main/l/lussekatter.jpg",
+        Category: "Sött",
+        Price: 39.90,
+        Quantity: 1,       
+        Description: "Lorem"
+    },
+    {
+        Id: "15",
+        Name: "Peppar Kakor",
+        Img: "https://res.cloudinary.com/coopsverige/image/upload/v1632490120/cloud/234240.jpg",
+        Category: "Sött",
+        Price: 29.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "16",
+        Name: "jul bullar",
+        Img: "https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/imagevaultfiles/id_63271/cf_259/julbullar.jpg",
+        Category: "Sött",
+        Price: 37.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "17",
+        Name: "Glögg",
+        Img: "https://www.unbooze.se/wp-content/uploads/2022/11/infused-liquid-glogg-blabar.png",
+        Category: "Dryck",
+        Price: 59.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "18",
+        Name: "Inläggningssill",
+        Img: "https://static.mathem.se/shared/images/products/large/07350126082743_c1n1.jpg",
+        Category: "Fisk",
+        Price: 30.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "19",
+        Name: "Vego bullar",
+        Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkvX-XIzo8pSWzds2OI0DQ_30NZKIDJt3Ow&usqp=CAU",
+        Category: "Vego",
+        Price: 50.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "20",
+        Name: "Jul Bröd",
+        Img: "https://sisselblom.se/wp-content/uploads/2015/03/julekake.jpg",
+        Category: "Bröd",
+        Price: 40.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "21",
+        Name: "Köttbullar",
+        Img: "https://crockpot.se/wp-content/uploads/2021/04/kottbulla_web-640x480.jpg",
+        Category: "Kött",
+        Price: 50.90,
+        Quantity: 1,       
+        Description: "Lorem"
+    },
+    {
+        Id: "22",
+        Name: "Lucia Katter",
+        Img: "https://www.recepten.se/bilder/recept/2/main/l/lussekatter.jpg",
+        Category: "Sött",
+        Price: 39.90,
+        Quantity: 1,       
+        Description: "Lorem"
+    },
+    {
+        Id: "23",
+        Name: "Peppar Kakor",
+        Img: "https://res.cloudinary.com/coopsverige/image/upload/v1632490120/cloud/234240.jpg",
+        Category: "Sött",
+        Price: 29.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "24",
+        Name: "jul bullar",
+        Img: "https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/imagevaultfiles/id_63271/cf_259/julbullar.jpg",
+        Category: "Sött",
+        Price: 37.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "25",
+        Name: "Glögg",
+        Img: "https://www.unbooze.se/wp-content/uploads/2022/11/infused-liquid-glogg-blabar.png",
+        Category: "Dryck",
+        Price: 59.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "26",
+        Name: "Inläggningssill",
+        Img: "https://static.mathem.se/shared/images/products/large/07350126082743_c1n1.jpg",
+        Category: "Fisk",
+        Price: 30.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "27",
+        Name: "Vego bullar",
+        Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkvX-XIzo8pSWzds2OI0DQ_30NZKIDJt3Ow&usqp=CAU",
+        Category: "Vego",
+        Price: 50.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
+    {
+        Id: "28",
+        Name: "Jul Bröd",
+        Img: "https://sisselblom.se/wp-content/uploads/2015/03/julekake.jpg",
+        Category: "Bröd",
+        Price: 40.90,
+        Quantity: 1,       
+        Description: "lorem"
+    },
 ];
