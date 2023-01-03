068ac6ca3add32c74685e3f48b9848b915428813
Mikael Rönnberg <mikaelronnberg@Mikaels-MacBook-Pro.local>
2023-01-03 12:14:37 +0100

    struktur i sass

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -66,7 +66,11 @@ function showCart(cart: IProduct[]) {
 function updateCart(newCart: IProduct[]) {
     let freshCart: IProduct[] = [];
     for(let i = 0; i < newCart.length; i++) {
-        freshCart.push(newCart[i]);
+        if(newCart[i].Quantity != 0){
+            freshCart.push(newCart[i]);
+        } else {
+            window.location.reload();
+        }
     }
     showCart(freshCart);
 }
